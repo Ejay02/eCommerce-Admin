cdeb1a62535d4cfce8671a4cb9577427efc212ba
Ejay02 <e.jae02@gmail.com>
2024-07-10 14:51:05 +0100

    WIP:Admin UI Setup

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -11,7 +11,7 @@
         <!-- Email -->
         <a-space direction="vertical" class="form-floating mb-3 w-100">
           <a-input
-            :value="email"
+            v-model:value="email"
             placeholder="name@example.com"
             id="email"
           />
@@ -22,18 +22,32 @@
           size="middle"
           class="form-floating mb-3 w-100"
         >
-          <!-- style="width: 100%" -->
           <a-input-password
-            :value="password"
+            v-model:value="password"
             placeholder="password"
             id="password"
           />
         </a-space>
+        <div class="d-flex justify-content-between">
+          <div class="col-auto">
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                id="autoSizingCheck2"
+              />
+              <label class="form-check-label" for="autoSizingCheck2">
+                Remember me
+              </label>
+            </div>
+          </div>
+          <router-link to="/forgot-password">Forgot password?</router-link>
+        </div>
         <!-- Button -->
         <router-link to="/admin">
           <div class="text-left">
             <button
-              class="btn border-0 px-3 py-2 text-white fw-bold w-100 text-center te"
+              class="btn border-0 px-3 py-2 text-white fw-bold w-100 text-center"
               type="submit"
             >
               Log In
@@ -41,6 +55,35 @@
           </div>
         </router-link>
       </form>
+
+      <!-- continue -->
+      <div class="or-continue">
+        <hr class="line" />
+        <span class="text-center capitalize">or continue with</span>
+        <hr class="line" />
+      </div>
+
+      <!-- social buttons -->
+      <div class="d-flex flex-wrap justify-content-between w-100">
+        <button
+          class="btn d-flex border-0 fw-bold text-center align-items-center"
+          type="submit"
+        >
+          <i class="fa-brands fa-google me-1"></i> Google
+        </button>
+        <button
+          class="btn d-flex border-0 fw-bold text-center align-items-center"
+          type="submit"
+        >
+          <i class="fa-brands fa-facebook me-1"></i> Facebook
+        </button>
+        <button
+          class="d-flex align-items-center btn border-0 fw-bold text-center"
+          type="submit"
+        >
+          <i class="fa-brands fa-github me-1"></i> Github
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -65,5 +108,29 @@ const onSubmit = () => {
 
 .btn {
   background-color: #ffd333;
+  width: 32%;
+  font-size: 14px;
+}
+
+.or-continue {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin: 1rem 0;
+  color: gray;
+}
+
+.line {
+  flex: 1;
+  border: 0;
+  height: 1px;
+  background: gray;
+}
+
+@media (max-width: 768px) {
+  .d-flex.flex-md-row {
+    flex-direction: column;
+  }
 }
 </style>
